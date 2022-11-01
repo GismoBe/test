80f9426be39674c7a16c74021c040f7b7bf3f6bc
GismoBe <tfrhonda@gmail.com>
2022-11-01 11:39:07 +0100

    alioth: Bringup Rice

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -14,6 +14,32 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
+# maintainer flag (one word only/no spacing)
+RICE_MAINTAINER := GismoBe
+RICE_CHIPSET := SM8250
+
+TARGET_OPTOUT_GOOGLE_TELEPHONY := true
+
+# AudioFx
+TARGET_EXCLUDES_AUDIOFX := true
+
+TARGET_USE_PIXEL_FINGERPRINT := true
+
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+RICE_PACKAGE_TYPE := PIXEL
+
+# gapps build flag, if not defined build type is vanilla
+WITH_GMS := true
+
+# Sushi Bootanimation (only 720/1080p/1440 supported. if not defined, bootanimation is google bootanimation)
+SUSHI_BOOTANIMATION := 1080
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := true
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
 PRODUCT_NAME := lineage_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
@@ -21,5 +47,3 @@ PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-BUILD_FINGERPRINT := POCO/alioth_global/alioth:12/SKQ1.211006.001/V13.0.3.0.SKHMIXM:user/release-keys
